f3333ae11875d94acd54b794ec285b0fcdce7628
Terrance Rose Jr <trose618@gmail.com>
2018-10-23 05:14:32 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,11 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  
+  holiday_hash.each do |season, holidays|
+    if holidays == :fourth_of_july
+      return holidays[1]
+    end
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
